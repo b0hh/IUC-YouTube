bb217079ddafa62d142472d136aa9bc62454f475
ydü <147338084+onxprod@users.noreply.github.com>
2023-10-19 22:58:33 +0300

    Delete yt_Channel.sql

--- yt_Channel.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Channel
-(
-	ID uniqueidentifier NOT NULL DEFAULT newid() PRIMARY KEY,
-	channelName varchar(128) NOT NULL,
-	channelDomainName varchar(128) NOT NULL unique,
-	channelCreatedAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-)
