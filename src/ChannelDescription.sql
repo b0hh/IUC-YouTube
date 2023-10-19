ac51f4e76809b863327221a0806825064d7c7825
ydü <147338084+onxprod@users.noreply.github.com>
2023-10-19 23:52:05 +0300

    Update and rename ChannelDescription.sql to yt_ChannelDescription.sql

--- src/ChannelDescription.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE ChannelDescription
-(
-	ID UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID() PRIMARY KEY,
-    _description TEXT NOT NULL DEFAULT "", -- desrciption on tanimli oldugu icin _ eklendi.
-)
---
+++ src/yt_ChannelDescription.sql
@@ -0,0 +1,5 @@
+CREATE TABLE ChannelDescription
+(
+	ID UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID() PRIMARY KEY,
+    _description TEXT NOT NULL DEFAULT ' ', -- desrciption on tanimli oldugu icin _ eklendi.
+)
