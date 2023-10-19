579a59daf77110e85f85dc6bf955a4d662a08ec7
ydü <147338084+onxprod@users.noreply.github.com>
2023-10-19 23:53:54 +0300

    Update and rename ShortsDescription.sql to yt_ShortsDescription.sql

--- src/ShortsDescription.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE ShortsDescription
-(
-	ID UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID() PRIMARY KEY,
-    _description TEXT NOT NULL DEFAULT "", -- desrciption on tanimli oldugu icin _ eklendi.
-)
