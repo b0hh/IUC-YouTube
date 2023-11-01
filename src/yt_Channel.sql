fdc2203ce18d46bf180a96fec26b951113c40cda
rqoto <barathalac@gmail.com>
2023-11-02 00:53:18 +0300

    domain name aciklayan yorum satiri guncellendi

--- src/yt_Channel.sql
+++ src/yt_Channel.sql
@@ -8,7 +8,7 @@ CREATE TABLE Channel
 (
 	ID UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID() PRIMARY KEY,
 	channel_name VARCHAR(128) NOT NULL,
-	channel_domain_name VARCHAR(128) NOT NULL UNIQUE, -- https://www.youtube.com/@user_name
+	channel_domain_name VARCHAR(128) NOT NULL UNIQUE, -- Örnek : https://www.youtube.com/@user_name -> @username hem kullanıcı adı hem de kanalı tanımlayan özel domain
 	channel_created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	channel_language INT FOREIGN KEY REFERENCES Languages(ID),
 	created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
