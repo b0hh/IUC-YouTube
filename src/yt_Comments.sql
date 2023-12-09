decba442e62391754f0a32fdbe4d37c1d4b85bb6
rqoto <barathalac@gmail.com>
2023-12-09 14:37:26 +0300

    yorumlar tablosuna is_pinned ve liked_by_creator alanlari eklendi

--- src/yt_Comments.sql
+++ src/yt_Comments.sql
@@ -1,12 +1,14 @@
 -- ↓ SQL TABLO GÖRÜNÜMÜ ↓
 
--- +----+----------+---------+
--- | ID | video_ID | comment |
--- +----+----------+---------+
+-- +----+----------+---------+-----------+------------------+
+-- | ID | video_ID | comment | is_pinned | liked_by_creator |
+-- +----+----------+---------+-----------+------------------+
 
 CREATE TABLE Comments 
 (
 	ID UNIQUEIDENTIFIER  DEFAULT newid() NOT NULL PRIMARY KEY,
 	video_ID UNIQUEIDENTIFIER NOT NULL FOREIGN KEY REFERENCES Video(ID),
 	comment TEXT NOT NULL,
+  is_pinned BIT DEFAULT 0, -- 0 -> sabitlenmemis / 1 -> sabitlenmis
+  liked_by_creator BIT DEFAULT 0, -- 0 -> icerik ureticisi begenmemis / 1 -> icerik urecticisi begenmis
 )
