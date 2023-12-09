c906b517a7cbd14ed3b7e1460b3f5e8bf1856d3a
rqoto <barat2370@gmail.com>
2023-12-09 22:14:02 +0300

    kanal tipleri tablosu eklendi

---
+++ src/ChannelTypes.sql
@@ -0,0 +1,11 @@
+-- ↓ SQL TABLO GÖRÜNÜMÜ ↓
+
+-- +----+-----------+
+-- | ID | type_name |
+-- +----+-----------+
+
+CREATE TABLE ChannelTypes
+(
+    ID INT NOT NULL IDENTITY(1,1),
+    type_name VARCHAR(64),
+)
