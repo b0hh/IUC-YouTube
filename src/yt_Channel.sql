83b65f58bcfd7bd08338707c3fc59bde77ed44d8
rqoto <barat2370@gmail.com>
2023-12-09 22:07:54 +0300

    kanal idsi hesap idsine baglandi

--- src/yt_Channel.sql
+++ src/yt_Channel.sql
@@ -6,9 +6,9 @@
 
 CREATE TABLE Channel
 (
-	ID UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID() PRIMARY KEY,
-	channel_name VARCHAR(128) NOT NULL,
-	channel_domain_name VARCHAR(128) NOT NULL UNIQUE, -- Örnek : https://www.youtube.com/@user_name -> @username hem kullanıcı adı hem de kanalı tanımlayan özel domain
+	ID UNIQUEIDENTIFIER NOT NULL FOREIGN KEY REFERENCES Account(ID),
+	channel_name VARCHAR(255) NOT NULL,
+	channel_domain_name VARCHAR(255) NOT NULL UNIQUE, -- Örnek : https://www.youtube.com/@user_name -> @username hem kullanıcı adı hem de kanalı tanımlayan özel domain
 	channel_created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	channel_language INT FOREIGN KEY REFERENCES Languages(ID),
 	created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
