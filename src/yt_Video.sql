62c9e3d56a99897ecc2856a9d6c0d6850d08f47d
rqoto <barat2370@gmail.com>
2023-12-10 23:14:47 +0300

    duration fieldi eklendi

--- src/yt_Video.sql
+++ src/yt_Video.sql
@@ -1,19 +1,20 @@
 -- ↓ SQL TABLO GÖRÜNÜMÜ ↓
 
--- +----+------------+-------+------------+-------+------------+---------+-------------------+------------+----------------+
--- | ID | channel_ID | title | created_at | likes | country_ID | privacy | video_description | video_type | video_category |
--- +----+------------+-------+------------+-------+------------+---------+-------------------+------------+----------------+
+-- +----+------------+-------+----------+------------+-------+------------+---------+-------------------+------------+----------------+
+-- | ID | channel_ID | title | duration | created_at | likes | country_ID | privacy | video_description | video_type | video_category |
+-- +----+------------+-------+----------+------------+-------+------------+---------+-------------------+------------+----------------+
 
 CREATE TABLE Video 
 (
    ID UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID() PRIMARY KEY,
    channel_ID UNIQUEIDENTIFIER NOT NULL FOREIGN KEY REFERENCES Channel(ID), 
+   duration INT NOT NULL,
    title VARCHAR NOT NULL DEFAULT GETDATE(),
    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
    likes INT NOT NULL DEFAULT 0 ,
    country_ID INT NOT NULL FOREIGN KEY REFERENCES Country(ID),
    privacy INT NOT NULL FOREIGN KEY REFERENCES Privacy(ID),
    video_description UNIQUEIDENTIFIER NOT NULL FOREIGN KEY REFERENCES VideoDescription(ID),
-   video_type BOOLEAN DEFAULT 0, -- 0 -> normal icerik  / 1 -> cocuklara ozel icerik
+   video_type BIT DEFAULT 0, -- 0 -> normal icerik  / 1 -> cocuklara ozel icerik
    video_category INT NOT NULL FOREIGN KEY REFERENCES VideoCategory(ID),
 )
