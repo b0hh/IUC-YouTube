c82ea2e4db88045d9021d1da5b31ac831b2075cb
rqoto <barat2370@gmail.com>
2023-12-25 23:33:15 +0300

    channel desc tablosuna baglanti eklendi

--- src/yt_ChannelDescription.sql
+++ src/yt_ChannelDescription.sql
@@ -1,11 +1,12 @@
 -- ↓ SQL TABLO GÖRÜNÜMÜ ↓
 
--- +----+--------------+
--- | ID | _description |
--- +----+--------------+
+-- +----+------------+--------------+
+-- | ID | channel_ID | _description | 
+-- +----+------------+--------------+
 
 CREATE TABLE ChannelDescription
 (
 	ID UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID() PRIMARY KEY,
-    _description TEXT NOT NULL DEFAULT ' ', -- desrciption on tanimli oldugu icin _ eklendi.
-)
+    channel_ID UNIQUEIDENTIFIER NOT NULL FOREIGN KEY REFERENCES Channel(ID),
+   _description TEXT NOT NULL DEFAULT ' ', -- desrciption on tanimli oldugu icin _ eklendi.
+)    
