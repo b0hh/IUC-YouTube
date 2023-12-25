02c2cf77abc7ebb9f16fb9833d4a9a425ca0ad01
rqoto <barat2370@gmail.com>
2023-12-25 23:38:38 +0300

    ilgili tablolara baglantilar eklendi

--- src/yt_ShortsDescription.sql
+++ src/yt_ShortsDescription.sql
@@ -8,5 +8,6 @@
 CREATE TABLE ShortsDescription
 (
 	ID UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID() PRIMARY KEY,
+    shorts_ID UNIQUEIDENTIFIER NOT NULL FOREIGN KEY REFERENCES Shorts(ID), 
     _description TEXT NOT NULL DEFAULT ' ', -- desrciption on tanimli oldugu icin _ eklendi.
 )
