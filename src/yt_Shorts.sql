10a06b1a21a6bad9afc107915b87fddec0ac82b3
rqoto <barat2370@gmail.com>
2024-01-03 22:26:24 +0300

    update

--- src/yt_Shorts.sql
+++ src/yt_Shorts.sql
@@ -1,16 +1,7 @@
--- ↓ SQL TABLO GÖRÜNÜMÜ ↓
-
--- +----+------------+-------+----------------+------------+---------------+------------+---------+
--- | ID | channel_ID | title | description_ID | like_count | dislike_count | created_at | privacy | 
--- +----+------------+-------+----------------+------------+---------------+------------+---------+
-
 CREATE TABLE Shorts
 (
     ID UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID() PRIMARY KEY,
     channel_ID UNIQUEIDENTIFIER NOT NULL FOREIGN KEY REFERENCES Channel(ID),
     title VARCHAR NOT NULL DEFAULT GETDATE(),
-    like_count INT NOT NULL DEFAULT 0,
-    dislike_count INT NOT NULL DEFAULT 0,
-    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     privacy_ID INT NOT NULL FOREIGN KEY REFERENCES Privacy(ID)
 )
