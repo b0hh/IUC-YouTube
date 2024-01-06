1425eec6ba46681aa9b4168a082f9f9c33ba0a62
bhh <142506536+rqoto@users.noreply.github.com>
2024-01-06 18:25:58 +0300

    Update yt_ShortsDescription.sql

--- src/yt_ShortsDescription.sql
+++ src/yt_ShortsDescription.sql
@@ -9,5 +9,5 @@ CREATE TABLE ShortsDescription
 (
 	ID UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID() PRIMARY KEY,
     shorts_ID UNIQUEIDENTIFIER NOT NULL FOREIGN KEY REFERENCES Shorts(ID), 
-    _description TEXT NOT NULL DEFAULT ' ', -- desrciption on tanimli oldugu icin _ eklendi.
+    _description VARCHAR(512) NOT NULL DEFAULT ' ', -- desrciption on tanimli oldugu icin _ eklendi.
 )
