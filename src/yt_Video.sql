c4f0a7b89e0a745f522fe9be40ba3e09b00ae1af
bhh <142506536+rqoto@users.noreply.github.com>
2024-01-06 18:26:38 +0300

    Update yt_Video.sql

--- src/yt_Video.sql
+++ src/yt_Video.sql
@@ -9,10 +9,10 @@ CREATE TABLE Video
    ID                UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID() PRIMARY KEY,
    channel_ID        UNIQUEIDENTIFIER NOT NULL FOREIGN KEY REFERENCES Channel(ID), 
    duration          INT NOT NULL,
-   title             VARCHAR NOT NULL DEFAULT GETDATE(),
+   title             VARCHAR(255) NOT NULL DEFAULT GETDATE(),
    privacy_ID           INT NOT NULL FOREIGN KEY REFERENCES Privacy(ID),
    video_type        BIT DEFAULT 0, -- 0 -> normal icerik  / 1 -> cocuklara ozel icerik
    --video_link'i sonradan alter table ile eklemek gerekiyor
-   video_link        VARCHAR NOT NULL UNIQUE FOREIGN KEY REFERENCES VideoLink(link)
+   video_link        VARCHAR(255) NOT NULL UNIQUE FOREIGN KEY REFERENCES VideoLink(link)
 )
 
