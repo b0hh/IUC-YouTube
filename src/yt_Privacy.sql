69b51b709ffd4aae1d9abdc0621db4e95cd999a6
bhh <142506536+rqoto@users.noreply.github.com>
2024-01-06 18:25:04 +0300

    Update yt_Privacy.sql

--- src/yt_Privacy.sql
+++ src/yt_Privacy.sql
@@ -7,5 +7,5 @@
 CREATE TABLE Privacy
 (
     ID INT PRIMARY KEY DEFAULT 0, -- 0 -> public / 1 -> unlisted / 2 -> private
-    privacy_type VARCHAR NOT NULL DEFAULT 'Public'
+    privacy_type VARCHAR(255) NOT NULL DEFAULT 'Public'
 )
