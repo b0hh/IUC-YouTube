a333cc819577695b7ed4c969a1c795e7b2911a99
ydü <147338084+onxprod@users.noreply.github.com>
2024-01-06 18:12:33 +0300

    Update yt_Channel.sql

--- src/yt_Channel.sql
+++ src/yt_Channel.sql
@@ -10,7 +10,6 @@ CREATE TABLE Channel
 	channel_name VARCHAR(255) NOT NULL,
 	channel_domain_name VARCHAR(255) NOT NULL UNIQUE, -- Örnek : https://www.youtube.com/@user_name -> @username hem kullanıcı adı hem de kanalı tanımlayan özel domain
 	channel_created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	channel_type INT FOREIGN KEY REFERENCES ChannelTypes(ID),
 	is_verified BIT NOT NULL DEFAULT 0, -- 0 -> Dogrulanmamis kanal / 1 -> dogrulanmis kanal 
 	--TODO channel kisisel bilgileri icin farkli tablo olusturulması
