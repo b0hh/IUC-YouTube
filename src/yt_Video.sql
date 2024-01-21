6b91d168f0c533a8112f7352e8c8221bd384c0a4
ydü <147338084+onxprod@users.noreply.github.com>
2024-01-21 13:13:32 +0300

    Update yt_Video.sql

--- src/yt_Video.sql
+++ src/yt_Video.sql
@@ -10,9 +10,10 @@ CREATE TABLE Video
    channel_ID        UNIQUEIDENTIFIER NOT NULL FOREIGN KEY REFERENCES Channel(ID), 
    duration          INT NOT NULL,
    title             VARCHAR(255) NOT NULL DEFAULT GETDATE(),
-   privacy_ID           INT NOT NULL FOREIGN KEY REFERENCES Privacy(ID),
+   privacy_ID        INT NOT NULL FOREIGN KEY REFERENCES Privacy(ID),
    video_type        BIT DEFAULT 0, -- 0 -> normal icerik  / 1 -> cocuklara ozel icerik
    --video_link'i sonradan alter table ile eklemek gerekiyor
    video_link        VARCHAR(255) NOT NULL UNIQUE FOREIGN KEY REFERENCES VideoLink(link)
+   language_ID       INT NOT NULL FOREIGN KEY REFERENCES Languages(ID),
 )
 
